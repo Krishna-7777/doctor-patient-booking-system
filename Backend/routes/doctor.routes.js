bc492ceeaa9a940d29e4fc011f687ca923581cb5
Krishna-7777 <kchhabraji13@gmail.com>
2023-06-21 19:17:32 +0530

    Delete Slot route for Doctors

--- Backend/routes/doctor.routes.js
+++ Backend/routes/doctor.routes.js
@@ -3,13 +3,22 @@ const { SlotsModel } = require("../models/slots.model");
 
 const doctorRoutes = express.Router();
 
-doctorRoutes.post('/createSlot', async (ask, give) => {
+doctorRoutes.post('/slot', async (ask, give) => {
     try {
         let slot = new SlotsModel(ask.body)
         await slot.save()
         give.send({msg:"Availability Slot has been Created."})
     } catch (error) {
         give.send({msg:"Error in Creating the Slots"})
+    }
+})
+
+doctorRoutes.delete('/slot/:id', async (ask, give) => {
+    try {
+        await SlotsModel.findByIdAndDelete(ask.params.id)
+        give.send({msg:"Slot has been Deleted."})
+    } catch (error) {
+        give.send({msg:"Error in Deleting the Slot."})
     }
 })
 
