336c1243aca20f0db3762d81b500c94695e53837
Krishna-7777 <kchhabraji13@gmail.com>
2023-06-22 09:49:25 +0530

    Added Login Page

---
+++ Frontend/scripts/login.js
@@ -0,0 +1,20 @@
+document.querySelector("form").addEventListener('submit',async(e)=>{
+    e.preventDefault()
+    let email=document.getElementById("email").value
+    let password=document.getElementById("password").value
+    let res=await fetch("https://doctor-patient-booking-system.onrender.com/user/login",{
+        method:"POST",
+        headers:{
+            "Content-Type":"Application/json"
+        },
+        body:JSON.stringify({
+            email,password
+        })
+    })
+    res=await res.json()
+    alert(res.msg)
+    localStorage.setItem("token",res.token)
+    localStorage.setItem("type",res.type)
+    localStorage.setItem("name",res.name)
+    window.location=`../pages/${res.type}.html`
+})
